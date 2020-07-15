8843446a563f99e48835169474dd6e00eeeee582
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2020-07-15 16:10:55 +0200

    One script to update all

---
+++ scripts/update_all_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Deployer tous les repos git sur toutes les machines
+
+# parallaxe2050-1
+/home/parallaxe2050/projets/parallaxe2050/scripts/update-p2050.sh
+# parallaxe2050-2
+ssh -t parallaxe2050@parallaxe2050-2.local sudo /home/parallaxe2050/projets/parallaxe2050/scripts/update-p2050.sh
+# parallaxe2050-3
+ssh -t parallaxe2050@parallaxe2050-3.local sudo /home/parallaxe2050/projets/parallaxe2050/scripts/update-p2050.sh
+# parallaxe2050-4
+ssh -t parallaxe2050@parallaxe2050-4.local sudo /home/parallaxe2050/projets/parallaxe2050/scripts/update-p2050.sh
+# parallaxe2050-5
+ssh -t parallaxe2050@parallaxe2050-5.local sudo /home/parallaxe2050/projets/parallaxe2050/scripts/update-p2050.sh
+
