5c56e58d9e73afb8012560dda9859486ab7e9aaf
marjolaine-ve <marjolaine.ve@gmail.com>
2020-08-13 17:46:11 +0200

    Configuration de l'émulation des cadges

--- index.js
+++ index.js
@@ -124,14 +124,26 @@ if (GLOBAL_CONFIG.rfid.behavior == "emulated") {
   // Testing for null data
   // rfid.extractTag("\n");
   // rfid.extractReader("\n");
-  // Testing for group A
+  // Testing for group A1 7ED72360 (énigme "Hardware" ou énigme "Code et pro")
+  rfid.extractTag("<TAG:7ED72360/><READER:1/>");
+  rfid.extractReader("<TAG:7ED72360/><READER:1/>");
+  scenario.setCurrentStepId("step-1");
+  // // Testing for group A2 5E3D621A (énigme "Code et prog" ou énigme "BDD et datas")
+  // rfid.extractTag("<TAG:5E3D621A/><READER:1/>");
+  // rfid.extractReader("<TAG:5E3D621A/><READER:1/>");
+  // scenario.setCurrentStepId("step-1");
+  // // Testing for group A3 0EAF4C60 (énigme "BDD et datas" ou énigme "Hardware")
+  // rfid.extractTag("<TAG:0EAF4C60/><READER:1/>");
+  // rfid.extractReader("<TAG:0EAF4C60/><READER:1/>");
+  // scenario.setCurrentStepId("step-1");
+  // // Testing for group A4 49426960 (énigme "Com digitale" ou énigme "Admin réseau")
   // rfid.extractTag("<TAG:49426960/><READER:1/>");
   // rfid.extractReader("<TAG:49426960/><READER:1/>");
   // scenario.setCurrentStepId("step-1");
-  // Testing for group A2 5E3D621A
-  rfid.extractTag("<TAG:5E3D621A/><READER:1/>");
-  rfid.extractReader("<TAG:5E3D621A/><READER:1/>");
-  scenario.setCurrentStepId("step-2");
+  // // Testing for group A5 5E68811A (énigme "Admin réseau" ou énigme "Com digitale")
+  // rfid.extractTag("<TAG:5E68811A/><READER:1/>");
+  // rfid.extractReader("<TAG:5E68811A/><READER:1/>");
+  // scenario.setCurrentStepId("step-1");
 
   // Testing for group B
   // rfid.extractTag("<TAG:CE4E2B60/><READER:2/>");
