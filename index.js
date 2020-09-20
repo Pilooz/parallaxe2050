a3eef974c126a5a392ed2b8ff1168cd4fccffd74
marjolaine-ve <marjolaine.ve@gmail.com>
2020-09-20 23:47:24 +0200

    [Admin réseau] Interface du typing

--- index.js
+++ index.js
@@ -208,13 +208,13 @@ if (GLOBAL_CONFIG.rfid.behavior == "emulated") {
   // rfid.extractReader("<TAG:7ED72360/><READER:1/>");
   // scenario.setCurrentStepId("step-1");
   // Testing for group A2 5E3D621A (énigme "Code et prog" ou énigme "BDD et datas") // UPDATE : "Admin réseau"
-  // rfid.extractTag("<TAG:5E3D621A/><READER:1/>");
-  // rfid.extractReader("<TAG:5E3D621A/><READER:1/>");
-  // scenario.setCurrentStepId("step-2");
-  // Testing for group A3 0EAF4C60 (énigme "BDD et datas" ou énigme "Code et prog") // UPDATE : "Code et prog"
-  rfid.extractTag("<TAG:0EAF4C60/><READER:1/>");
-  rfid.extractReader("<TAG:0EAF4C60/><READER:1/>");
+  rfid.extractTag("<TAG:5E3D621A/><READER:1/>");
+  rfid.extractReader("<TAG:5E3D621A/><READER:1/>");
   scenario.setCurrentStepId("step-1");
+  // Testing for group A3 0EAF4C60 (énigme "BDD et datas" ou énigme "Code et prog") // UPDATE : "Code et prog"
+  // rfid.extractTag("<TAG:0EAF4C60/><READER:1/>");
+  // rfid.extractReader("<TAG:0EAF4C60/><READER:1/>");
+  // scenario.setCurrentStepId("step-1");
   // Testing for group A4 49426960 (énigme "Com digitale" ou énigme "Admin réseau")
   // rfid.extractTag("<TAG:49426960/><READER:1/>");
   // rfid.extractReader("<TAG:49426960/><READER:1/>");
