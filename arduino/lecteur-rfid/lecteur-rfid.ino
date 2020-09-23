9195d6a23049526025e4eef9e0202af4b4f97882
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-09-23 09:43:36 +0200

    Bugfix on Fastled usage

--- arduino/lecteur-rfid/lecteur-rfid.ino
+++ arduino/lecteur-rfid/lecteur-rfid.ino
@@ -1,58 +1,86 @@
 /*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read new NUID from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
- * Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * @license Released into the public domain.
- * 
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- */
+   --------------------------------------------------------------------------------------------------------------------
+   Example sketch/program showing how to read new NUID from a PICC to serial.
+   --------------------------------------------------------------------------------------------------------------------
+   This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+
+   Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+   Reader on the Arduino SPI interface.
+
+   When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+   then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+   you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+   will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
+   when removing the PICC from reading distance too early.
+
+   @license Released into the public domain.
+
+   Typical pin layout used:
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   SPI SS      SDA(SS)      10            53        D10        10               10
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+*/
 
 #include <SPI.h>
 #include <MFRC522.h>
 #include <FastLED.h>
 
-
+#define BRIGHTNESS 255
 #define NUM_LEDS 6
 #define DATA_PIN 3
 CRGB leds[NUM_LEDS];
 
 #define SS_PIN 10
 #define RST_PIN 9
- 
+
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-MFRC522::MIFARE_Key key; 
+MFRC522::MIFARE_Key key;
 
-// Init array that will store new NUID 
+// Init array that will store new NUID
 byte nuidPICC[4];
+boolean scanned = false;
+long current_millis = 0;
+long previous_millis = 0;
 
-void setup() { 
+/**
+   Helper routine to dump a byte array as hex values to Serial.
+*/
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? "0" : "");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+void red() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // Turn our current led on to white, then show the leds
+    leds[i] = CRGB::Red;
+  }
+  FastLED.show();
+}
+void green() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // Turn our current led on to white, then show the leds
+    leds[i] = CRGB::Green;
+  }
+  FastLED.show();
+}
+
+void setup() {
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
+  rfid.PCD_Init(); // Init MFRC522
 
-   for (byte i = 0; i < 6; i++) {
+  for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
 
@@ -62,17 +90,23 @@ void setup() {
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
-    for(int i = 0; i < NUM_LEDS; i = i + 1) {
-      // Turn our current led on to white, then show the leds
-      leds[i] = CRGB::Red;
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();      
-   }
-   #define BRIGHTNESS          100
+  red(); scanned = false;
   Serial.println("<MSG:READY/>");
 }
- 
+
 void loop() {
+  //  current_millis = millis();
+  //  if (scanned && current_millis - previous_millis > 2000) {
+  //    red();
+  //    scanned = false;
+  //    previous_millis = current_millis;
+  //  }
+  EVERY_N_MILLISECONDS (30000) {
+    if (scanned) {
+      red();
+      scanned = false;
+    }
+  }
 
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! rfid.PICC_IsNewCardPresent())
@@ -87,38 +121,35 @@ void loop() {
   //Serial.println(rfid.PICC_GetTypeName(piccType));
 
   // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
     Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+  if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+      rfid.uid.uidByte[1] != nuidPICC[1] ||
+      rfid.uid.uidByte[2] != nuidPICC[2] ||
+      rfid.uid.uidByte[3] != nuidPICC[3] ) {
     //Serial.println(F("A new card has been detected."));
 
     // Store NUID into nuidPICC array
     for (byte i = 0; i < 4; i++) {
       nuidPICC[i] = rfid.uid.uidByte[i];
     }
-   
+
     //Serial.println(F("The NUID tag is:"));
     Serial.print(F("<TAG:"));
     printHex(rfid.uid.uidByte, rfid.uid.size);
-  //  Serial.print("><READER:1>");
+    Serial.print("/><READER:1/>");
     //Serial.print(F("In dec: "));
     //printDec(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
 
-    for(int i = 0; i < NUM_LEDS; i = i + 1) {
-      // Turn our current led on to white, then show the leds
-      leds[i] = CRGB::Green;
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();      
-   }
+    green();
+    scanned = true;
+
   }
   //else Serial.println(F("Card read previously."));
 
@@ -127,25 +158,4 @@ void loop() {
 
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
-}
-
-
-/**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? "0" : "");
-    Serial.print(buffer[i], HEX);
-  }
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? "0" : "");
-    Serial.print(buffer[i], DEC);
-  }
 }
