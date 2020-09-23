40182e3b5275adbe3b8aff968d6c933a289ede5a
Trompettesib <60519602+Trompettesib@users.noreply.github.com>
2020-09-23 20:44:49 +0200

    changing some thing in the girlduino like changing cable managment so changing pin and refactoring the code for leds

--- arduino/girlduino/girlduino.ino
+++ arduino/girlduino/girlduino.ino
@@ -1,71 +1,71 @@
 #include <FastLED.h>
 
 
-#define NUM_LEDS 6
-#define DATA_PIN 3
+#define NUM_LEDS      6
+#define DATA_PIN      5
+#define COLOR_ORDER GRB
+#define LED_TYPE WS2811
+#define BRIGHTNESS    100
 CRGB leds[NUM_LEDS];
 
-int bouton = 2;
-int sortie01 = 3;
+int bouton = 3;
+int sortie01 = 2;
 int sortie02 = 4;
 
 void setup() {
   // put your setup code here, to run once:
-FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  turn_led_to_color_rgb(0, 0, 0);
 
-    for(int i = 0; i < NUM_LEDS; i = i + 1) {
-      // Turn our current led on to white, then show the leds
-      leds[i] = CRGB::Black;
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();      
-   }
-   #define BRIGHTNESS          100
-   pinMode (bouton,INPUT);
-   pinMode (sortie01,OUTPUT);
-   pinMode (sortie02,OUTPUT);
+  pinMode       (A0, INPUT_PULLDOWN);
+  pinMode       (A1, INPUT_PULLDOWN);
+  pinMode       (A2, INPUT_PULLDOWN);
+  pinMode       (A3, INPUT_PULLDOWN);
+  pinMode       (A4, INPUT_PULLDOWN);
+  pinMode       (A5, INPUT_PULLDOWN);
+
+  pinMode       (bouton,INPUT_PULLDOWN);
+  pinMode       (sortie01,OUTPUT);
+  pinMode       (sortie02, OUTPUT);
+  digitalWrite  (sortie01, LOW);
+  digitalWrite  (sortie02, LOW);
 }
 
 void loop() {
 
-int valA0 = analogRead (A0);
-int valA1 = analogRead (A1); 
-int valA2 = analogRead (A2); 
-int valA3 = analogRead (A3); 
-int valA4 = analogRead (A4);
-int valA5 = analogRead (A5);  
+  int valA0 = analogRead (A0);
+  int valA1 = analogRead (A1);
+  int valA2 = analogRead (A2);
+  int valA3 = analogRead (A3);
+  int valA4 = analogRead (A4);
+  int valA5 = analogRead (A5);
 
-int valBouton = digitalRead(bouton);
+  int valBouton = digitalRead(bouton);
 
-if (valBouton == 1)
-{
+  if (valBouton == 1) {
 
-if (valA0 == xxx && valA1 == xxx && valA2 == xxx && valA3 == xxx && valA4 == xxx && valA5 == xxx &&)
-{
-  digitalWrite (sortie01,HIGH);   
+    if (valA0 == xxx && valA1 == xxx && valA2 == xxx && valA3 == xxx && valA4 == xxx && valA5 == xxx &&) {
+      digitalWrite (sortie01,HIGH);
+      delay(100);
+    }
+    else if (valA0 == xxx && valA1 == xxx && valA2 == xxx && valA3 == xxx && valA4 == xxx && valA5 == xxx &&) {
+      digitalWrite (sortie02,HIGH);
+      delay(100);
+    }
+    else {
+      turn_led_to_color_rgb(255, 0, 0);
+      digitalWrite (sortie01,LOW);
+      digitalWrite (sortie02,LOW);
+    }
+  } else {
+    digitalWrite (sortie01,LOW);
+    digitalWrite (sortie02,LOW);
+  }
 }
 
-else if (valA0 == xxx && valA1 == xxx && valA2 == xxx && valA3 == xxx && valA4 == xxx && valA5 == xxx &&)
-{
-  digitalWrite (sortie02,HIGH);   
-}
-
-else {
-  
-    for(int i = 0; i < NUM_LEDS; i = i + 1) {
-      // Turn our current led on to white, then show the leds
-      leds[i] = CRGB::Red;
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();      
-        digitalWrite (sortie01,LOW); 
-        digitalWrite (sortie02,LOW); 
-   }
-}
-}
-
-else {
-  digitalWrite (sortie01,LOW); 
-  digitalWrite (sortie02,LOW);   
-}
-
-
+void turn_led_to_color_rgb(int r, int g, int b) {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(r, g, b);
+  }
+  FastLED.show();
 }
