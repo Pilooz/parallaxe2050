760eedd09403274d5c1e43a87e1603304eae8484
Trompettesib <60519602+Trompettesib@users.noreply.github.com>
2020-10-08 15:05:23 +0200

    making the code more beautiful

--- arduino/module_manivelle/test_bandeau_led/test_bandeau_led.ino
+++ arduino/module_manivelle/test_bandeau_led/test_bandeau_led.ino
@@ -5,6 +5,7 @@
 #define DATA_PIN 4
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
+#define MAX_RGB_VALUE 255
 
 #define MANIVELLE_PIN A0
 #define TIME_BETWEEN_EACH_INCREMENTATION 1000
@@ -19,14 +20,12 @@ ParallaxeCom message;
 int counter;
 bool activity;
 bool buzz;
+int rgb_value_between_each_led;
 unsigned long currentMillis;
 unsigned long lastMillis1;
 unsigned long lastMillis2;
 
 void setup() {
-  // Init Sérial
-  Serial.begin(9600);
-
   // Init buzzer, relay and manivelle pins
   pinMode (RELAY_PIN, OUTPUT);
   pinMode (BUZZER_PIN, OUTPUT);
@@ -38,12 +37,17 @@ void setup() {
   currentMillis = 0;
   lastMillis1 = 0;
   lastMillis2 = 0;
+  rgb_value_between_each_led = ceil(MAX_RGB_VALUE/NUM_LEDS);
+  if (rgb_value_between_each_led == 0)
+    rgb_value_between_each_led = 1;
 
   // Init led gestion
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  for (int r = 255, g = 0, nb_led = 0; nb_led < NUM_LEDS; nb_led++, r -= 15, g += 15)
-    leds[nb_led] = CRGB(r, g, 0);
+  turn_off();
   counter = 0;
+
+  // Init Serial
+  Serial.begin(9600);
 
   // Init messaging
   while(!Serial);
@@ -70,7 +74,7 @@ void loop() {
   }
 
   if ((currentMillis - lastMillis1) >= TIME_BETWEEN_EACH_INCREMENTATION) {
-    if ((analogRead(MANIVELLE_PIN) >= 1000) && (counter < 17)) {
+    if ((analogRead(MANIVELLE_PIN) >= 1000) && (counter < NUM_LEDS)) {
       counter++;
     }
     lastMillis1 = currentMillis;
@@ -100,7 +104,7 @@ void loop() {
 }
 
 void refresh(int nb_led_max) {
-  for (int r = 255, g = 0, nb_led = 0; nb_led < nb_led_max; nb_led++, r -= 15, g += 15)
+  for (int r = MAX_RGB_VALUE, g = 0, nb_led = 0; nb_led < nb_led_max; nb_led++, r -= rgb_value_between_each_led, g += rgb_value_between_each_led)
     leds[nb_led] = CRGB(r, g, 0);
   for (int nb_led = nb_led_max; nb_led < NUM_LEDS; nb_led++)
     leds[nb_led] = CRGB(0, 0, 0);
