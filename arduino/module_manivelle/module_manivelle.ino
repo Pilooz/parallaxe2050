f53cadecaab1c143cf7f4b181835f83a462c069f
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-11-16 17:46:10 +0100

    name

--- arduino/module_manivelle/module_manivelle.ino
+++ arduino/module_manivelle/module_manivelle.ino
@@ -1,170 +1,122 @@
-
-// code forké sur un code initial d'equalizer
-
-#include <FastLED.h>
+#include "FastLED.h"
 #include "protocole_parallaxe2050.h"
 
-
-#define LED_PIN     4
-#define NUM_LEDS    17
-#define BRIGHTNESS  10
-#define LED_TYPE    WS2811
+#define NUM_LEDS 17
+#define DATA_PIN 4
+#define LED_TYPE WS2811
 #define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS];
+#define MAX_RGB_VALUE 255
 
-#define UPDATES_PER_SECOND 100
+#define MANIVELLE_PIN A0
+#define TIME_BETWEEN_EACH_INCREMENTATION 1000
+#define TIME_BETWEEN_EACH_DECREMENTATION 250
+#define RELAY_PIN 8
+#define BUZZER_PIN 10
+
+CRGB leds[NUM_LEDS];
 
 ParallaxeCom message;
 
-//  INPUT SETUP
-int pin_manivelle = A0;
-int relay = 8;
-int buzzer = 10;
-
-
-// STANDARD VISUALIZER VARIABLES
-int loop_max = 0;
-int k = 255; // COLOR WHEEL POSITION
-int decay = 0; // HOW MANY MS BEFORE ONE LIGHT DECAY
-int decay_check = 0;
-long pre_react = 0; // NEW SPIKE CONVERSION
-long react = 0; // NUMBER OF LEDs BEING LIT
-long post_react = 0; // OLD SPIKE CONVERSION
-
-// RAINBOW WAVE SETTINGS
-int wheel_speed = 3;
-
-// create the bool value for buzzing
-bool buzz;
-// create the bool value for activity
+int counter;
 bool activity;
+bool buzz;
+int rgb_value_between_each_led;
+unsigned long currentMillis;
+unsigned long lastMillis1;
+unsigned long lastMillis2;
 
-void setup()
-{
-  // SERIAL SETUP
-  Serial.begin(9600);
 
-  pinMode (relay, OUTPUT);
-  pinMode (buzzer, OUTPUT);
-  // LED LIGHTING SETUP
-  delay( 3000 ); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-
-  // CLEAR LEDS
-  for (int i = 0; i < NUM_LEDS; i++)
-    leds[i] = CRGB(0, 0, 0);
+void refresh(int nb_led_max) {
+  for (int r = MAX_RGB_VALUE, g = 0, nb_led = 0; nb_led < nb_led_max; nb_led++, r -= rgb_value_between_each_led, g += rgb_value_between_each_led)
+    leds[nb_led] = CRGB(r, g, 0);
+  for (int nb_led = nb_led_max; nb_led < NUM_LEDS; nb_led++)
+    leds[nb_led] = CRGB(0, 0, 0);
   FastLED.show();
+}
 
-  // INPUT SETUP
-  pinMode(pin_manivelle, INPUT);
+void turn_off() {
+  for (int nb_led = 0; nb_led < NUM_LEDS; nb_led++)
+    leds[nb_led] = CRGB(0, 0, 0);
+  FastLED.show();
+}
 
-  // interaction with computer
+void setup() {
+  // Init buzzer, relay and manivelle pins
+  pinMode (RELAY_PIN, OUTPUT);
+  pinMode (BUZZER_PIN, OUTPUT);
+  pinMode (MANIVELLE_PIN, INPUT);
+
+  // contrôle
   buzz = false;
   activity = false;
+  currentMillis = 0;
+  lastMillis1 = 0;
+  lastMillis2 = 0;
+  rgb_value_between_each_led = ceil(MAX_RGB_VALUE/NUM_LEDS);
+  if (rgb_value_between_each_led == 0)
+    rgb_value_between_each_led = 1;
 
+  // Init led gestion
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  turn_off();
+  counter = 0;
 
-  // arduino just finish setup
+  // Init Serial
+  Serial.begin(9600);
+
+  // Init messaging
+  while(!Serial);
+  message.send("NAME", "MANIVELLE");
+  delay(5000);
   message.send("MSG", "READY");
 }
 
-// FUNCTION TO GENERATE COLOR BASED ON VIRTUAL WHEEL
-// https://github.com/NeverPlayLegit/Rainbow-Fader-FastLED/blob/master/rainbow.ino
-CRGB Scroll(int pos) {
-  CRGB color (0, 0, 0);
-  if (pos < 85) {
-    color.g = 0;
-    color.r = 255;
-    color.b = 0;
-  } else if (pos > 85 && pos < 170) {
-    color.g = 255;
-    color.r = 255;
-    color.b = 0;
-  } else if (pos > 170 && pos < 256) {
-    color.b = 0;
-    color.g = 255;
-    color.r = 0;
-  }
-  return color;
-}
+void loop() {
+  currentMillis = millis();
 
-// FUNCTION TO GET AND SET COLOR
-// THE ORIGINAL FUNCTION WENT BACKWARDS
-// THE MODIFIED FUNCTION SENDS WAVES OUT FROM FIRST LED
-// https://github.com/NeverPlayLegit/Rainbow-Fader-FastLED/blob/master/rainbow.ino
-void rainbow()
-{
-  for (int i = NUM_LEDS - 1; i >= 0; i--) {
-    if (i < react)
-      leds[i] = Scroll((i * 256 / 50 + k) % 256);
-    else
-      leds[i] = CRGB(0, 0, 0);
-  }
-  FastLED.show();
-}
-
-void loop()
-{
-  // Message handling
   if (message.isKey("CMD")) {
-    if (message.val() == "START") {
+    if (message.val() == "START" || message.val() == "MANIVELLE_ON") {
       buzz = true;
       activity = true;
       message.ack_ok();
     }
-    if (message.val() == "STOP") {
+    if (message.val() == "STOP" || message.val() == "MANIVELLE_OFF") {
       buzz = false;
       activity = false;
       message.ack_ok();
     }
   }
 
-  if (!buzz)
-    analogWrite (buzzer, 0);
-  if (!activity)
-    digitalWrite (relay, HIGH);
-
-  int sig_manivelle = analogRead(pin_manivelle); // ADD x2 HERE FOR MORE SENSITIVITY
-
-  if (sig_manivelle > 100)
-  {
-    digitalWrite (relay, HIGH);
-    analogWrite (buzzer, 0);
-    pre_react = ((long)NUM_LEDS * (long)sig_manivelle) / 1023L; // TRANSLATE AUDIO LEVEL TO NUMBER OF LEDs
-
-    if (pre_react > react) // ONLY ADJUST LEVEL OF LED IF LEVEL HIGHER THAN CURRENT LEVEL
-      react = pre_react;
-
-    //Serial.print(sig_manivelle);
-    //Serial.print(" -> ");
-    //Serial.println(pre_react);
+  if ((currentMillis - lastMillis1) >= TIME_BETWEEN_EACH_INCREMENTATION) {
+    if ((analogRead(MANIVELLE_PIN) >= 1000) && (counter < NUM_LEDS)) {
+      counter++;
+    }
+    lastMillis1 = currentMillis;
   }
-  else
-  {
-    if (buzz)
-      analogWrite (buzzer, 600);
-    if (activity)
-      digitalWrite (relay, LOW);
+  if ((currentMillis - lastMillis2) >= TIME_BETWEEN_EACH_DECREMENTATION) {
+    if ((analogRead(MANIVELLE_PIN) <= 1000) && (counter > 0)) {
+      counter--;
+    }
+    lastMillis2 = currentMillis;
   }
 
-
-  rainbow(); // APPLY COLOR
-
-  k = k - wheel_speed; // SPEED OF COLOR WHEEL
-  if (k < 0) // RESET COLOR WHEEL
-    k = 255;
-
-  // REMOVE LEDs
-  decay_check++;
-  if (decay_check > decay)
-  {
-    decay_check = 0;
-    if (react > 0)
-      react--;
+  if (buzz && activity) {
+    if (counter == 0) {
+      analogWrite(BUZZER_PIN, 600);
+      digitalWrite(RELAY_PIN, LOW);
+    } else {
+      analogWrite(BUZZER_PIN, 0);
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+  } else {
+    analogWrite(BUZZER_PIN, 0);
+    digitalWrite(RELAY_PIN, HIGH);
   }
 
-  //delay(1);
+  if (activity)
+    refresh(counter);
 }
+
 
 void serialEvent() {
   message.receive();
